cbf67bb7121a265819ff9b0d392383dd39b44ee6
Unyime <jumbounyime396@gmail.com>
2026-05-29 01:33:28 +0100

    fix port for render deployment

--- backend/server.js
+++ backend/server.js
@@ -75,7 +75,7 @@ app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
